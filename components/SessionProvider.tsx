fbc628f800e86acbcba759839bfc7dd72be56c07
Darwin3300 <recursiveconsciousnessbip@gmail.com>
2026-04-23 21:04:15 +0000

    fix: move use client directive to top of file

--- components/SessionProvider.tsx
+++ components/SessionProvider.tsx
@@ -1,5 +1,7 @@
-import { SessionProvider } from "next-auth/react";
 "use client";
+
+import { SessionProvider } from "next-auth/react";
+
 export default function Provider({ children }: { children: React.ReactNode }) {
       return <SessionProvider>{children}</SessionProvider>;
 }
